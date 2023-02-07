827ab0ea7823208aa46cb97d180a80105fc1fe2b
seri lee <serirucy@gmail.com>
2023-02-08 08:01:04 +0900

    popup

--- js/js_index.js
+++ js/js_index.js
@@ -8,4 +8,47 @@ window.addEventListener("scroll", ()=>{
   }else{
     header.classList.add('headerOn');
   }
+});
+
+const popup = document.querySelector('.popup-con');
+const popupOut = popup.querySelector('span');
+const popupIframe = popup.querySelector('iframe');
+const sec2 = document.querySelector('section.sec2');
+const sec2buttons = sec2.querySelector('.buttons');
+const sec2buttonsBtn = sec2buttons.querySelector('p');
+const sec3 = document.querySelector('section.sec3');
+const sec3buttons = sec3.querySelector('.buttons');
+const sec3buttonsBtn = sec3buttons.querySelectorAll('p');
+
+sec2buttonsBtn.addEventListener('click', ()=>{
+  popup.classList.add('popupOn');
+  popupIframe.setAttribute('src', 'https://lucyseri.github.io/vixtorm/');
+  popupIframe.setAttribute('width', '1920px');
+  popupIframe.setAttribute('height', '860px');
+  popupIframe.style.borderRadius = "0";
+})
+sec3buttons.addEventListener('click', (e)=>{
+  sec3buttonsBtn.forEach((el, idx)=>{
+    if(e.target == el){
+      popup.classList.add('popupOn');
+      popupIframe.setAttribute('src', 'https://lucyseri.github.io/jellycat_project/');
+      if(idx == 0){
+        popupIframe.setAttribute('width', '1920px');
+        popupIframe.setAttribute('height', '860px');
+        popupIframe.style.borderRadius = "0";
+      }else if(idx == 1){
+        popupIframe.setAttribute('width', '768px');
+        popupIframe.setAttribute('height', '1024px');
+        popupIframe.style.borderRadius = 25 + "px";
+        
+      }else if(idx == 2){
+        popupIframe.setAttribute('width', '480px');
+        popupIframe.setAttribute('height', '910px');
+        popupIframe.style.borderRadius = 25 + "px";
+      }
+    }
+  })
+})
+popupOut.addEventListener('click', ()=>{
+  popup.classList.remove('popupOn');
 })
